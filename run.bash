c6b926d0b16fe14ddefaea6c8f94c7dd8fc8dbd9
Paul Tarvydas <guitarvydas@gmail.com>
2021-06-05 10:52:59 -0400

    using m4 for boilerplate

--- run.bash
+++ run.bash
@@ -5,22 +5,20 @@ set -x
 echo >_.pl
 
 echo '*** building transpilers ***'
+
 ../grasem/run.bash md2block.grasem >_.js
 cat foreign.js _.js >_md2block.js
 
 ../grasem/run.bash block2brace.grasem >_.js
 cat foreign.js _.js >_block2brace.js
 
-../grasem/run.bash brace2fb.grasem >_.js
-cat foreign.js _.js >_brace2fb.js
+m4 <brace2fb.grasem >_.grasem
+../grasem/run.bash _.grasem
+# ../grasem/run.bash _.grasem >_.js
+# cat foreign.js _.js >_brace2fb.js
 
 
 
 node _md2block.js < $target.md > _.block
 node _block2brace.js < _.block > _.brace
-node _brace2fb.js < _.brace > ${target}.fb
-
-# awk -f name-mangling.awk < _.pl | sort > fb.pl
-# cat fb.pl
-
-
+# node _brace2fb.js < _.brace > ${target}.fb
