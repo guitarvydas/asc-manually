4997558d585fb4de216212956ae44475c3b429ec
Paul Tarvydas <guitarvydas@gmail.com>
2021-06-05 12:25:50 -0400

    chop run.bash into pieces

---
+++ build.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+../grasem/run.bash md2block.grasem >_.js
+cat foreign.js _.js >_md2block.js
+
+../grasem/run.bash block2brace.grasem >_.js
+cat foreign.js _.js >_block2brace.js
+
+m4 <brace2fb.grasem >_.grasem
+../grasem/run.bash _.grasem >_.js
+cat foreign.js _.js >_brace2fb.js
--- run.bash
+++ run.bash
@@ -5,19 +5,8 @@ echo >_.pl
 
 echo '*** building transpilers ***'
 
-../grasem/run.bash md2block.grasem >_.js
-cat foreign.js _.js >_md2block.js
+./build.bash
 
-../grasem/run.bash block2brace.grasem >_.js
-cat foreign.js _.js >_block2brace.js
+./md2fb.bash app
 
-m4 <brace2fb.grasem >_.grasem
-../grasem/run.bash _.grasem >_.js
-cat foreign.js _.js >_brace2fb.js
-
-
-
-node _md2block.js < $target.md > _.block
-node _block2brace.js < _.block > _.brace
-node _brace2fb.js < _.brace > ${target}.fb
 cat ${target}.fb
