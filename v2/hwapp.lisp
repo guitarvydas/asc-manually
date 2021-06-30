f2501d53c11de1afb2b701e7d4ecdc24a94b573d
Paul Tarvydas <guitarvydas@gmail.com>
2021-06-30 00:54:20 -0400

    relative-id (rid) engine using EA (effective address)
    set flag to create components during def
    
    
    wip: break looking for error "must be component" in input()
    
    
    fetch-raw & component name
    
    
    wip: break

--- v2/hwapp.lisp
+++ v2/hwapp.lisp
@@ -1,12 +1,12 @@
-(def (rid "hwapp" "c" "_nonamekind"))
-(input (ref-component (rid "hwapp" "c" "_nonamekind")) "i" (def (rid (rid "hwapp" "c" "_nonamekind") "i" "in")))
-(output (ref-component (rid "hwapp" "c" "_nonamekind")) "o" (def (rid (rid "hwapp" "c" "_nonamekind") "o" "out")))
+(defasc (rid "hwapp" "c" "_nonamekind"))
+(input (rid (rid "hwapp" "c" "_nonamekind") "i" "in"))
+(output (rid (rid "hwapp" "c" "_nonamekind") "o" "out"))
 
-(def "hwapp")
-(input (ref-component "hwapp") "i" (def (rid "hwapp" "i" "in")))
-(output (ref-component "hwapp") "o" (def (rid "hwapp" "o" "out")))
-(contains (ref-component "hwapp") "c" (clone (ref (rid "hwapp" "c" "_nonamekind"))))
-(connection (ref-component "hwapp") "x" (def (rid "hwapp" "x" 1)) (on (ref (rid "hwapp" "i" "in")) (send 'downward (ref (rid (rid "hwapp" "c" "inner") "i" "in")) m)))
-(connection (ref-component "hwapp") "x" (def (rid "hwapp" "x" 2)) (on (ref (rid (rid "hwapp" "c" "inner") "o" "out")) (send 'upward (ref (rid "hwapp" "o" "out")) m)))
+(defasc "hwapp")
+(input (rid "hwapp" "i" "in"))
+(output (rid "hwapp" "o" "out"))
+(contains (rid "hwapp" "c" "inner") (rid "hwapp" "c" "_nonamekind"))
+(connection (rid "hwapp" "x" 1) (on (rid "hwapp" "i" "in") (send 'downward (rid (rid "hwapp" "c" "inner") "i" "in") m)))
+(connection (rid "hwapp" "x" 2) (on (rid (rid "hwapp" "c" "inner") "o" "out") (send 'upward (rid "hwapp" "o" "out") m)))
 
 
