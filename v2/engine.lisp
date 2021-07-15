1b90e4dd4b0b06fdacfd5ac47201b32193fdc2ce
Paul Tarvydas <guitarvydas@gmail.com>
2021-07-15 06:47:47 -0400

    wip: alpha engine.lisp

--- v2/engine.lisp
+++ v2/engine.lisp
@@ -1,154 +1,79 @@
-;; "rid" means "relative id"
-;; rid is a 3-tuple - 1) component, 2) namespace, 3) name
+(defun def% (rid)
+  (cond ((eq 'yes (is-leaf (path rid)))
+	 (design-rule-component-must-not-exist (path rid))
+	 (def-component-raw (path rid)))
+	((eq 'no is-leaf (path rid))
+	 (def-path-component (path rid))
+	 (let ((path-component (get (path rid)))
+	       (def (create-rid path-component (ns rid) (name rid))))))
+	(t (panic (format nil "def %a" rid)))))
 
-(defclass relative-id ()
-  ((path :accessor path :initarg :path)
-   (ns :accessor ns :initarg :namespace)
-   (id :accessor id :initarg :id)))
+(defun get% (rid)
+  (cond ((eq 'yes (is-leaf (path rid)))
+	 (design-rule-component-name-must-exist (path rid))
+	 (let ((component-name (lookup-component (path rid))))
+	   (design-rule-namespace-must-exist component-name (ns rid))
+	   (design-rule-value-must-exist component-name (ns rid) (name rid))
+	   (lookup-value component-name (ns rid) (name rid))))
 
-(defmacro rid (path ns id)
-  `(make-instance 'relative-id :path ,path :namespace ,ns :id ,id))
-
-(defclass child-descriptor ()
-  ((name :accessor name :initarg :name)
-   (static :accessor static :initarg :static)
-   (dynamic :accessor dynamic :initarg :dynamic :initform nil)))
-
-(defclass connection-descriptor ()
-  ((name :accessor name :initarg :name)       ;; a rid
-   (tag :accessor tag :initarg :tag)          ;; a rid
-   (action :accessor action :initarg :action))) ;; a lambda
-
-(defun make-child (name kind)
-  (make-instance 'child-descriptor :name name :static kind))
-
-(defun make-connection (name tag action)
-  (make-instance 'connection-descriptor :name name :tag tag :action action))
-
-;;;;;;;;; rid utilities
-
-(defclass component ()
-  ;; each component has 6 namespaces i,o,x,c,k,n
-  ;; (input, output, connection, component, other-names, resp.)
-  ((namespaces :accessor namespaces :initform (make-hash-table :test 'equal))
-   (name :accessor name :initarg :name)))
-
-(defun make-component (name)
-  (let ((c (make-instance 'component :name name)))
-    (setf (gethash "i" (namespaces c)) (make-hash-table :test 'equal))
-    (setf (gethash "o" (namespaces c)) (make-hash-table :test 'equal))
-    (setf (gethash "x" (namespaces c)) (make-hash-table :test 'equal))
-    (setf (gethash "c" (namespaces c)) (make-hash-table :test 'equal))
-    (setf (gethash "k" (namespaces c)) (make-hash-table :test 'equal))
-    (setf (gethash "n" (namespaces c)) (make-hash-table :test 'equal))
-    c))
+	((eq 'no (is-leaf (path rid)))
+	 (let ((component (get (path rid))))
+	   (design-rule-path-must-be-a-component component)
+	   (get% (create-rid component ns name))))
 	
-(defmethod static ((c component))
-  c)
+	(t (panic (format nil "get %a" rid)))))
 
-(defparameter *component-table* (make-hash-table :test 'equal))
+(defun set% (rid val)
+  (cond ((eq 'yes is-leaf (path rid))
+	 (design-rule-component-must-exist (path rid))
+	 (let ((component-name (lookup-component (path rid))))
+	   (design-rule-namespace-must-exist component-name (ns rid))
+	   (design-rule-value-must-not-exist component-name (ns rid) (name rid))
+	   (set-value-raw component-name (ns rid) (name rid))))
 
-(defun lookup (name)
-  (gethash name *component-table*))
+	((eq 'no is-leaf (path rid))
+	 (let ((component (get% (path rid))))
+	   (design-rule-path-must-be-a-component component)
+	   (set% (create-rid component (ns rid) (name rid)))))
 
-(defun create-component (name)
-  (setf (gethash name *component-table*) (make-component name)))
+	(t (panic (format nil "set %a" rid)))))
 
-(defun %design-rule-failure (message)
-  (let ((s (concatenate 'string "design rule failure: " message)))
-    (error s)))
 
-;; see rid-dsl
-(defparameter *create-flag* nil) ;; dynamic parameter used in ea-raw and fetch-raw, if t, create paths and don't flag missing paths as errors
 
-(defun component-namespace-p (ns)
-  (string= "c" ns))
+(defparameter *top-level-components* (make-hash-table :test 'equal))
 
-(defmethod fetch-raw (c ns id)
-  (let ((result (gethash id (gethash ns (namespaces c)))))
-    (when (and (null result) *create-flag* (component-namespace-p ns))
-      (setf result (make-component id))
-      (setf (gethash id (gethash ns (namespaces c))) result))
-    result))
+(defun design-rule-component-must-not-exist (cname)
+  (multiple-value-bind (c success)
+      (gethash *top-level-components* cname)
+    (declare (ignore c))
+    (unless (not success)
+      (design-rule-fail (format nil "component must not exist ~a" cname)))))
 
-(defmethod getter ((rid relative-id))
-  (with-slots (ns id) rid
-    (let ((c (ea rid)))
-      (let ((result (fetch-raw c ns id)))
-        result))))
+(defun design-rule-component-name-must-exist (cname)
+  (multiple-value-bind (c success)
+      (gethash *top-level-components* cname)
+    (declare (ignore c))
+    (unless (not success)
+      (design-rule-fail (format nil "component name exist ~a" cname)))))
 
-(defmethod setter ((rid relative-id) val)
-  (with-slots (ns id) rid
-    (let ((c (ea rid)))
-      (multiple-value-bind (_ success)
-          (gethash id (gethash ns (namespaces c)))
-        (declare (ignore _))
-        (when success
-          (%design-rule-failure "must not already have a value"))
-        (setf (gethash id (gethash ns (namespaces c))) val)))))
+(defun design-rule-namespace-must-exist (c ns)
+  (multiple-value-bind (namespace success)
+      (gethash ns c)
+    (declare (ignore ns))
+    (unless success
+      (design-rule-fail (format nil "namespace must exist ~a" (list c ns))))))
 
-(defmethod defasc ((rid relative-id))
-  (let ((*create-flag* t))
-    (ea rid)))
+(defun design-rule-value-must-not-exist (c ns name)
+  (multiple-value-bind (v success)
+      (gethash name (gethash ns c))
+    (unless (not success)
+      (design-rule-fail (format nil "value must not exist ~a" (list c ns name))))))
+(defun design-rule-value-must-exist (c ns name)
+  (multiple-value-bind (v success)
+      (gethash name (gethash ns c))
+    (unless success
+      (design-rule-fail (format nil "value must exist ~a" (list c ns name))))))
 
-(defmethod ea-raw ((rid relative-id))
-  (cond ((stringp (path rid))
-	 (let ((lu (lookup (path rid))))
-	   (unless lu 
-             (cond (*create-flag* 
-                    (create-component (path rid)))
-                   (t (%design-rule-failure "must not be empty"))))
-           (let ((result (lookup (path rid))))
-             result)))
-	(t
-	 (let ((ref (path rid)))
-	   (unless (string= "c" (ns ref))
-	     (%design-rule-failure "must contain a component namespace reference"))
-	   (let ((child (getter ref)))
-	     (let ((result (static child)))
-	       result))))))
-
-(defmethod ea ((rid relative-id))
-  (let ((result (ea-raw rid)))
-    (unless (eq 'component (type-of result))
-      (%design-rule-failure "must be a component"))
-    result))
-	
-;;;;;;;;;
-
-(defun error-if (expr estring)
-  (when expr
-    (error estring)))
-
-(defun error-if-not (expr estring)
-  (error-if (not expr) estring))
-
-;;;;;;;;;;;; engine
-
-(defun def-kind (rid)
-  (with-slots (path ns id) rid)
-  (def-ok-if-exists path)
-  (def-ok-if-exists 
-;; (defun new-component (name)
-;;   (defasc (make-instance 'relative-id :path name :namespace "" :id "")))
-
-;; (defun def-asc (rid) 
-;;   (def-ok-if-exists rid
-;;   (def-error-if-exists (iport-rid)))
-
-;; (defun def-input (iport-rid) 
-;;   (def-ok-if-exists (path iport-rid))
-;;   (def-error-if-exists (iport-rid)))
-
-;; (defun def-output (oport-rid) 
-;;   (def-ok-if-exists (path oport-rid))
-;;   (def-error-if-exists (oport-rid)))
-
-;; (defun text (rid str)
-;;   (setter rid str))
-
-;; (defun connection (name port fn)
-;;   (setter name (make-connection name port fn)))
-
-;; (defun contains (parent-rid child-name child-kind)
-;;   (setter parent-rid (make-child child-name child-kind)))
+(defun design-rule-path-must-be-a-component (c)
+  (unless (typeof c 'component)
+    (design-rule-fail (format nil "path must be a component ~a" c))))
